1bd2a940dbf32b60f67fedf85a5671b955b26726
Stacy Johns <stacykjohns@icloud.com>
2019-07-19 13:34:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    return array[0].to_s
+  when 2
+    string = array[0] + " and " + array[1]
+  else
+    string = String.new
+    array.each_with_index do |item, index|
+      string += "and " + item if index == array.size - 1
+      break if index == array.size - 1
+      string += item + ", "
+    end
+    return string
+  end
 end
