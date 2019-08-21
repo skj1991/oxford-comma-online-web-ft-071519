4493dedb718446221597f124618ec577c03f2bff
Stacy Johns <stacykjohns@icloud.com>
2019-08-21 23:26:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def oxford_comma(array)
   case array.size
   when 1
@@ -5,11 +6,17 @@ def oxford_comma(array)
   when 2
     string = array[0] + " and " + array[1]
   else
-    string = String.new
+    string = ""
     array.each_with_index do |item, index|
-      string += "and " + item if index == array.size - 1
-      break if index == array.size - 1
-      string += item + ", "
+      if index == array.size - 1
+        string += "and " + item
+      else
+        string += item + ", "
+      end
+
+      #string += "and " + item if index == array.size - 1 # if the index we're on is the last index in the array
+      #break if index == array.size - 1
+      #string += item + ", "
     end
     return string
   end
